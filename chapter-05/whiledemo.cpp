a50f39ea33caf7204e485301ff56bacec42e3c97
Rodrigo Eleuterio <roeleuss@gmail.com>
2021-07-30 00:11:40 -0300

    aprimora whiledemo.

--- chapter-05/whiledemo.cpp
+++ chapter-05/whiledemo.cpp
@@ -4,12 +4,20 @@ using namespace std;
 
 int main(int numberOfArgs, char* args[]) {
     int loopCount;
+    int initialCount;
+
     cout << "Enter loop count: ";
     cin >> loopCount;
+
+    initialCount = loopCount;
 
     while (loopCount-- > 0) {
         cout << "Only " << loopCount << " loops to go" << endl;
     }
+
+    do {
+        cout << "Count " << ++loopCount << endl;
+    } while (loopCount < initialCount);
 
     cout << "Press Enter to continue..." << endl;
     cin.ignore(10, '\n');
