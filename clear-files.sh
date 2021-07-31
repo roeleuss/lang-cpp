a007f16f231e91c320514539eedaa4071d268078
Rodrigo Eleuterio <roeleuss@gmail.com>
2021-07-31 11:29:49 -0300

    atualiza clear-files.sh para excluir os arquivos especificos em qualquer subpastas dentro das pastas part*

--- clear-files.sh
+++ clear-files.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo rm **/*.bin **/*.exe **/*.o? **/*.s?
+find ./part* -name "*.exe" -exec rm {} + -o -name "*.bin" -exec rm {} + -o -name "*.s?" -exec rm {} + -o -name "*.o?" -exec rm {} +
